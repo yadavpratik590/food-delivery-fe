bdb3bbec1705c5d6ac7b6bcfee734e631b39bb3d
Pratik <yadavpratik590@gmail.com>
2024-02-04 13:10:24 +0530

    initial-setup

---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+        <section className='-z-50 banner relative flex flex-col justify-center items-center '>
+            
+            <div className='w-[50vw] z-10 text-center'>
+                <p className='text-2xl lg:text-7xl font-bold z-10 py-5'> King Food</p>
+                <p className='z-10 text-gray-300 text-xl lg:text-4xl'>
+                    Taste the Convenience : Food , Fast and Delivered.
+                </p>
+            </div>
+
+        </section>
+    </div>
+  )
+}
+
+export default HomePage
